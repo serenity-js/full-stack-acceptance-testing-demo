e56a3c27d5620abf4d12bbb06b412ec726b389a9
Jan Molak <jan.molak@smartcodeltd.co.uk>
2020-01-26 19:33:29 +0000

    Initial commit

---
+++ spec/example.spec.ts
@@ -0,0 +1,16 @@
+import { Ensure, includes } from '@serenity-js/assertions';
+import { actorCalled, engage } from '@serenity-js/core';
+import { Navigate, UseAngular, Website } from '@serenity-js/protractor';
+import { Actors } from '../src';
+
+describe('serenity-js.org website', () => {
+
+    beforeEach(() => engage(new Actors()));
+
+    it(`tells people what they're reading @wip`, () =>
+        actorCalled('Jasmine').attemptsTo(
+            UseAngular.disableSynchronisation(),
+            Navigate.to('https://serenity-js.org'),
+            Ensure.that(Website.title(), includes('Serenity/JS')),
+        ));
+});
